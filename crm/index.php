cba16d37077b51d469d4620ea5b53369c8f7781f
noufal <noufalnasar11@gmail.com>
2022-06-20 23:26:07 +0530

    crm ticketing system

---
+++ crm/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CRM Project using PHP and MySQL</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light  fixed-top">
+    <div class="container-fluid"style=" margin-left: 4%;">
+      <a class="navbar-brand" href="#"><img src="./assets/img/djo.png" alt="" style="width:9rem; height:20% margin-left: 25%; " ></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav " style="margin-left: 29%;">
+          <li class="nav-item ">
+            <a class="nav-link text-light" href="registration.php"><strong>User Sign Up</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-5 text-light" href="login.php"><strong>User Log In</strong></a>
+          </li>
+
+            <li class="nav-item">
+            <a class="nav-link ml-5 text-light " href="admin/"><strong>Admin</strong></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+ </div>
+  </header> -->
+   <header >
+    
+      <div >
+        <div class="row">
+          <div class="col-6  col-xs-12">
+          <img src="./assets/img/banner.png" alt="" style="width: 1534px; height:700px;">
+          <div class="container" style=" margin-top: -500px;  margin-left: 10%; ">
+        <h1 class="text-light"style="font-size: 3.7rem; "><strong>CRM Ticketing System</strong></h1>
+        <h5 class=" mt-4 text-light" style="width: 32rem; line-height: 1.9;">Learn how to resolve support tickets faster and boost customer satisfaction.CRM software  works as a tool to help with contact management, sales management, productivity, and more.</h5>
+        <a href="registration.php" class="btn btn-light btn-lg rounded-pill mt-5 text-primary" style="font-size: 1.2rem; font-weight:500; " >User Signup</a>
+      </div>
+         
+
+          </div>
+          <div class="col-6   col-xs-12">
+          <img src="./assets/img/box_img.png" alt="" style="width: 500px; height:600px; margin-left: 20%;  margin-top: 20%;">
+ 
+
+          </div>
+        </div>
+       
+      </div>
+    
+   
+  </header>
+
+
+
+  
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
